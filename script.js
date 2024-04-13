f0f10719ad28c3a14b0d8c7756d6aff9555e84ae
Daniel Calzada Rubio <152646646+saddaniiii@users.noreply.github.com>
2024-04-13 03:06:13 +0200

    Section, Formulario, Estilos + Funcionamiento JS
    Se ha añadido el funcionamiento con JavaScript

---
+++ script.js
@@ -0,0 +1,38 @@
+generarContraseña();
+
+function generarContraseña() {
+    const button = document.getElementById('button');
+    const contraseñaGenerada = document.getElementById('password');
+    const caracteres = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'á', 'é', 'í', 'ó', 'ú', 'ü',
+    '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<',
+    '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+
+    button.addEventListener('click', function() {
+        const numero = Math.floor(Math.random() * 13) + 8;
+        let contraseña = '';
+        for (let i = 0; i < numero; i++) {
+            const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+            const caracterAleatorio = caracteres[indiceAleatorio];
+            contraseña += caracterAleatorio;
+        }
+        contraseñaGenerada.value = contraseña;
+        copiaralPortapapeles(contraseña);
+    })
+}
+
+function copiaralPortapapeles(text) {
+    const copy = document.getElementById('copy');
+    let messageCopy = document.getElementById('message-copy');
+
+    copy.addEventListener('click', function() {
+        const input = document.createElement('input');
+        input.value = text;
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand('copy');
+        document.body.removeChild(input);
+    })
+}
